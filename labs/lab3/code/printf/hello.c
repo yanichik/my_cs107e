ba332a0f19bf98912cf53c9ba83de7911a40a394
yanichik <brunshte@gmail.com>
2019-01-09 19:26:21 -0800

    added labs

---
+++ labs/lab3/code/printf/hello.c
@@ -0,0 +1,14 @@
+#include "timer.h"
+#include "uart.h"
+#include "printf.h"
+
+void main (void)
+{
+/*    uart_init();
+    timer_delay(2);
+    while(1) {
+        printf( "hello, laptop\n" );
+        timer_delay(1);
+    }*/
+    printf("%0x\n", *FSEL2);
+}
