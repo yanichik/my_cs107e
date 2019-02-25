a25c4d4b4a85774917c9f6d7ea9f79ec78c6f12d
yanichik <brunshte@gmail.com>
2019-02-24 18:09:00 -0800

    added TIMER addresses

--- assignments/ass2/timer.h
+++ assignments/ass2/timer.h
@@ -1,7 +1,19 @@
 #ifndef TIMER_H_INCLUDED
 #define TIMER_H_INCLUDED
 
+#define TIMER_BASE 0x20203000UL
+
+/*volatile*/ unsigned int* CS = (unsigned int*)(TIMER_BASE);
+/*volatile*/ unsigned int* CLO = (unsigned int*)(TIMER_BASE + 0x04);
+/*volatile*/ unsigned int* CHI = (unsigned int*)(TIMER_BASE + 0x08);
+/*volatile*/ unsigned int* C0 = (unsigned int*)(TIMER_BASE + 0xC);
+/*volatile*/ unsigned int* C1 = (unsigned int*)(TIMER_BASE + 0x10);
+/*volatile*/ unsigned int* C2 = (unsigned int*)(TIMER_BASE + 0x14);
+/*volatile*/ unsigned int* C3 = (unsigned int*)(TIMER_BASE + 0x18);
+
+
 unsigned int timer_get_time();
 void delay_us(unsigned int n);
+
 
 #endif
